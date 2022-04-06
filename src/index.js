3449f1cb57699d350038ae0cc09f9a87f9f61a49
tsmith2077 <tsmith2077@gmail.com>
2022-04-05 21:05:14 -0600

    Working restaurant website using javascript modules, webpack with a Dark Souls Theme.

---
+++ src/index.js
@@ -0,0 +1,307 @@
+import './styles.css';
+
+import Chef from './img/chef.jpg';
+import Sif from './img/sif.jpg';
+import Rat from './img/rat.jpg';
+import Snake from './img/snake.jpg';
+import Dung from './img/dung.jpg';
+import Chicken from './img/chicken.jpg';
+import Flask from './img/flask.jpg';
+import Gwyn from './img/gwyn.jpg';
+
+// Event handler for tabs
+function selectHomeTab() {
+    homePageContent.style.cssText = "display: flex;"
+    menuPageContent.style.cssText = "display: none;"
+    contactPageContent.style.cssText = "display: none;"
+
+    const allTabBtns = document.querySelectorAll('.tab');
+    allTabBtns.forEach(tab => {
+        if (!tab.classList.contains('home')) {
+            tab.classList.remove('activeTab');
+        } else {
+            tab.classList.add('activeTab');
+        }
+})
+};
+
+function selectMenuTab() {
+    homePageContent.style.cssText = "display: none;"
+    menuPageContent.style.cssText = "display: flex;"
+    contactPageContent.style.cssText = "display: none;"
+
+    const allTabBtns = document.querySelectorAll('.tab');
+    allTabBtns.forEach(tab => {
+        if (!tab.classList.contains('menu')) {
+            tab.classList.remove('activeTab');
+        } else {
+            tab.classList.add('activeTab');
+        }
+})
+};
+
+function selectContactTab() {
+    homePageContent.style.cssText = "display: none;"
+    menuPageContent.style.cssText = "display: none;"
+    contactPageContent.style.cssText = "display: flex;"
+
+    const allTabBtns = document.querySelectorAll('.tab');
+    allTabBtns.forEach(tab => {
+        if (!tab.classList.contains('contact')) {
+            tab.classList.remove('activeTab');
+        } else {
+            tab.classList.add('activeTab');
+        }
+})
+};
+
+const container = document.createElement("body");
+container.id = 'container';
+document.body = container;
+
+//Displays the header
+function header() {
+    const headerElement = document.createElement('div');
+
+    const header = document.createElement('div');
+    header.textContent = "Dark Souls Eats";
+    header.classList.add('header');
+    headerElement.appendChild(header);
+
+    const subheader = document.createElement('p')
+    subheader.textContent = "Soulsweet you'll die and want to come back for more!";
+    subheader.classList.add('subheader');
+    header.appendChild(subheader);
+
+    return headerElement;
+}
+
+const headerElement = document.createElement('div');
+headerElement.id = 'headerElement';
+container.appendChild(headerElement);
+headerElement.appendChild(header());
+
+// Displays the tabs
+function displayTabs() {
+    const mainContent = document.createElement('div');
+    mainContent.classList.add('tabBar');
+
+    const homeTab = document.createElement('button');
+    homeTab.textContent = "Home";
+    homeTab.classList.add('tab', 'home', 'activeTab');
+    homeTab.setAttribute("role", "tab")
+    homeTab.onclick = selectHomeTab;
+    mainContent.appendChild(homeTab);
+
+    const menuTab = document.createElement('button');
+    menuTab.textContent = "Menu";
+    menuTab.classList.add('tab', 'menu');
+    menuTab.setAttribute("role", "tab")
+    menuTab.onclick = selectMenuTab;
+    mainContent.appendChild(menuTab);
+
+    const contactTab = document.createElement('button');
+    contactTab.textContent = "Contact Us";
+    contactTab.classList.add('tab', 'contact');
+    contactTab.setAttribute("role", 'tab')
+    contactTab.onclick = selectContactTab;
+    mainContent.appendChild(contactTab);
+
+
+    return mainContent;
+}
+ 
+const allTabs = document.createElement('div');
+allTabs.id = 'allTabs';
+container.appendChild(allTabs);
+allTabs.appendChild(displayTabs());
+
+
+// Home Page Content
+function displayHomePage() {
+    const homePageContent = document.createElement('div')
+    homePageContent.classList.add('homePageContent');
+
+    const homePageHeader = document.createElement('div');
+    homePageHeader.classList.add('homePageHeader');
+    homePageHeader.textContent = "Whether you're the The Chosen Undead, the Bearer of the Curse, " + 
+    "or the Ashen One, Dark Souls Eats has the cuisine to make your last meal a great one!";
+    homePageContent.appendChild(homePageHeader)
+
+    const imageHolder = document.createElement('div');
+    imageHolder.classList.add('imageHolder');
+    homePageContent.appendChild(imageHolder);
+
+    const chefImgCaption = document.createElement('div');
+    chefImgCaption.classList.add('chefImgCaption');
+    chefImgCaption.setAttribute('style', 'white-space: pre;');
+    chefImgCaption.textContent = 'Meet the Chef\r\n';
+    chefImgCaption.textContent += 'This demon used his time in the Asylum to hone his cooking skills.';
+    imageHolder.appendChild(chefImgCaption)
+
+    const chefImg = new Image();
+    chefImg.src = Chef;
+    chefImg.classList.add('chefImg');
+    chefImgCaption.appendChild(chefImg);
+
+    const sifImgCaption = document.createElement('div');
+    sifImgCaption.classList.add('sifImgCaption');
+    sifImgCaption.textContent = 'Pets are Welcome!';
+    imageHolder.appendChild(sifImgCaption);
+
+    const sifImg = new Image();
+    sifImg.src = Sif;
+    sifImg.classList.add('sifImg');
+
+    sifImgCaption.appendChild(sifImg);
+
+    return homePageContent;
+}
+
+const homePageContent = document.createElement('div');
+homePageContent.id = 'homePageContent';
+container.appendChild(homePageContent);
+homePageContent.appendChild(displayHomePage());
+
+
+// Menu Page Content
+function displayMenuPage() {
+    const menuPageContent = document.createElement('div');
+    menuPageContent.classList.add('menuPageContent');
+
+    // Rat Menu Item
+    const ratHolder = document.createElement('div');
+    ratHolder.classList.add('ratHolder');
+    menuPageContent.appendChild(ratHolder);
+
+    const ratImgCaption = document.createElement('div');
+    ratImgCaption.setAttribute('style', 'white-space: pre;');
+    ratImgCaption.classList.add('ratImgCaption');
+    ratImgCaption.textContent = "Fried Sewer Rats\r\n";
+    ratImgCaption.textContent += 'We finally caught those pesky rats and fried them up for your pleasure.\r\n(We are not responsible for bloodloss.)';
+    ratHolder.appendChild(ratImgCaption);
+
+    const ratImg = new Image();
+    ratImg.src = Rat;
+    ratImg.classList.add('ratImg');
+    ratHolder.appendChild(ratImg);
+
+    // Snake Menu Item
+    const snakeHolder = document.createElement('div');
+    snakeHolder.classList.add('snakeHolder');
+    menuPageContent.appendChild(snakeHolder);
+
+    const snakeImgCaption = document.createElement('div');
+    snakeImgCaption.setAttribute('style', 'white-space: pre;');
+    snakeImgCaption.classList.add('snakeImgCaption');
+    snakeImgCaption.textContent = "Fried Frampt and Kaathe Bites\r\n";
+    snakeImgCaption.textContent += 'These slippery serperts are fried, and then tossed into a delicous breading.';
+    snakeHolder.appendChild(snakeImgCaption);
+
+    const snakeImg = new Image();
+    snakeImg.src = Snake;
+    snakeImg.classList.add('snakeImg');
+    snakeHolder.appendChild(snakeImg);
+
+    // Dung Menu Item
+    const dungHolder = document.createElement('div');
+    dungHolder.classList.add('dungHolder');
+    menuPageContent.appendChild(dungHolder);
+
+    const dungImgCaption = document.createElement('div');
+    dungImgCaption.setAttribute('style', 'white-space: pre;');
+    dungImgCaption.classList.add('dungImgCaption');
+    dungImgCaption.textContent = "Dung\r\n";
+    dungImgCaption.textContent += 'For you dung eaters out there,\r\n for you, we have a big wet pile of poo.';
+    dungHolder.appendChild(dungImgCaption);
+
+    const dungImg = new Image();
+    dungImg.src = Dung;
+    dungImg.classList.add('dungImg');
+    dungHolder.appendChild(dungImg);
+
+    // Chicken Tenders Menu Item
+    const chickenHolder = document.createElement('div');
+    chickenHolder.classList.add('chickenHolder');
+    menuPageContent.appendChild(chickenHolder);
+
+    const chickenImgCaption = document.createElement('div');
+    chickenImgCaption.setAttribute('style', 'white-space: pre;');
+    chickenImgCaption.classList.add('chickenImgCaption');
+    chickenImgCaption.textContent = "Chicken Tenders with Fries\r\n";
+    chickenImgCaption.textContent += 'No respectable restaurant is complete without Chicken Tenders.';
+    chickenHolder.appendChild(chickenImgCaption);
+
+    const chickenImg = new Image();
+    chickenImg.src = Chicken;
+    chickenImg.classList.add('chickenImg');
+    chickenHolder.appendChild(chickenImg);
+
+    // Flask Menu Item
+    const flaskHolder = document.createElement('div');
+    flaskHolder.classList.add('flaskHolder');
+    menuPageContent.appendChild(flaskHolder);
+
+    const flaskImgCaption = document.createElement('div');
+    flaskImgCaption.setAttribute('style', 'white-space: pre;');
+    flaskImgCaption.classList.add('flaskImgCaption');
+    flaskImgCaption.textContent = "Estus Flask\r\n";
+    flaskImgCaption.textContent += 'A Delicous Estus Flask to wash down your meal\r\n will have you feeling better than ever.';
+    flaskHolder.appendChild(flaskImgCaption);
+
+    const flaskImg = new Image();
+    flaskImg.src = Flask;
+    flaskImg.classList.add('flaskImg');
+    flaskHolder.appendChild(flaskImg);
+
+    return menuPageContent;
+}
+
+const menuPageContent = document.createElement('div');
+menuPageContent.id = 'menuPageContent';
+container.appendChild(menuPageContent);
+menuPageContent.appendChild(displayMenuPage());
+
+// Contact Us Content
+function displayContactPage() {
+    const contactPageContent = document.createElement('div');
+    contactPageContent.classList.add('contactPageContent');
+
+    // Gwyn manager display
+    const gwynHolder = document.createElement('div');
+    gwynHolder.classList.add('gwynHolder');
+    contactPageContent.appendChild(gwynHolder);
+
+    const gwynImgCaption = document.createElement('div');
+    gwynImgCaption.setAttribute('style', 'white-space: pre;');
+    gwynImgCaption.classList.add('gwynImgCaption');
+    gwynImgCaption.textContent = "Meet the Owner\r\n";
+    gwynImgCaption.textContent += 'Gwyn, Lord of Cinder';
+    gwynHolder.appendChild(gwynImgCaption);
+
+    const gwynImg = new Image();
+    gwynImg.src = Gwyn;
+    gwynImg.classList.add('gwynImg');
+    gwynHolder.appendChild(gwynImg);
+
+    const contactInfoHolder = document.createElement('div');
+    contactInfoHolder.classList.add('contactInfoHolder');
+
+    const contactInfo = document.createElement('div');
+    contactInfo.setAttribute('style', 'white-space: pre;');
+    contactInfo.classList.add('contactInfo');
+    contactInfo.textContent = "Dark Souls Eats\r\n\r\n";
+    contactInfo.textContent += "Now with 3 locations: \r\nKindom of Lodran, Drangelic, Kingdom of Lothric\r\n\r\n";
+    contactInfo.textContent += "Email: gitgud@pleasehelp.com";
+    contactInfoHolder.appendChild(contactInfo);
+
+    contactPageContent.appendChild(contactInfoHolder);
+
+
+    return contactPageContent;
+}
+
+const contactPageContent = document.createElement('div');
+contactPageContent.id = 'contactPageContent';
+container.appendChild(contactPageContent);
+contactPageContent.appendChild(displayContactPage());
